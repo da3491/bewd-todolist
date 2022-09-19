28ada51c2757bca8c981767f348b74669af797ac
Harry Chen <harrychenca@gmail.com>
2022-09-19 08:02:10 +0000

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # get '*path' => 'homepage#index'
+
+  root 'homepage#index'
+
   get 'tasks' => 'tasks#index'
   post 'tasks' => 'tasks#create'
   delete 'tasks/:id' => 'tasks#destroy'
   put '/tasks/:id/mark_complete' => 'tasks#mark_complete'
   put '/tasks/:id/mark_active' => 'tasks#mark_active'
+
+  get '*path' => 'homepage#index'
 end
